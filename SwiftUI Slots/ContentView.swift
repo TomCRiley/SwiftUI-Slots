4ac34b296ced7e5b12e965f9250342bdce5fcd96
Tom Riley <tom.riley@outlook.com>
2022-06-25 10:27:42 +0100

    Finished UI elements structure and layout

--- SwiftUI Slots/ContentView.swift
+++ SwiftUI Slots/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var score = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Text("SwiftUI Slots!")
+                .font(.largeTitle)
+                .padding()
+            
+            Spacer()
+            
+            Text(String(score))
+            
+            Spacer()
+            
+            HStack() {
+                Image("apple")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("apple")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("apple")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            Spacer()
+            
+            Button {
+        
+            } label: {
+                Text("Spin")
+            }
+            .padding(.horizontal, 25.0)
+            .padding(.vertical, 6.0)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+
+            .cornerRadius(15)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+
+            
+        }
     }
 }
 
