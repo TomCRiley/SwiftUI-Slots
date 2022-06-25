5e9cc3db505e1de1157a6e4d189686ecd695ac9f
Tom Riley <tom.riley@outlook.com>
2022-06-25 09:56:42 +0100

    Initial Commit

---
+++ SwiftUI Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI Slots
+//
+//  Created by Tom Riley on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
